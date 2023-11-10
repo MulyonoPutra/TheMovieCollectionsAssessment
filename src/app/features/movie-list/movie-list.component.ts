2a6e047195c0a045e35a3c0f359732a6bb0ce45e
Mulyono Putra <50153639+MulyonoPutra@users.noreply.github.com>
2023-11-10 14:21:13 +0700

    fix: remove unused component

--- src/app/features/movie-list/movie-list.component.ts
+++ src/app/features/movie-list/movie-list.component.ts
@@ -3,13 +3,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { MovieService } from 'src/app/core/services/movie.service';
-import { MovieTrendingComponent } from './pages/movie-trending/movie-trending.component';
 import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [CommonModule, ComponentsModule, MovieTrendingComponent],
+  imports: [CommonModule, ComponentsModule],
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss'],
 })
@@ -34,8 +33,6 @@ export class MovieListComponent implements OnInit, OnDestroy {
   setActiveTab(index: number): void {
     this.activeTabIndex = index;
   }
-
-
 
   findAllTrendings() {
     this.moviesService.findTrendingMovies('day').subscribe({
