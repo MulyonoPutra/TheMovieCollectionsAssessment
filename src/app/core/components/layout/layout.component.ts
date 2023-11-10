e64c905bce4a724c649857eb13132662397eb821
Mulyono Putra <50153639+MulyonoPutra@users.noreply.github.com>
2023-11-11 05:10:22 +0700

    create: Footer

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
@@ -7,7 +8,7 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 @Component({
 	selector: 'app-layout',
 	standalone: true,
-	imports: [CommonModule, RouterModule, HeaderComponent, SidebarComponent],
+	imports: [CommonModule, RouterModule, HeaderComponent, SidebarComponent, FooterComponent],
 	templateUrl: './layout.component.html',
 	styleUrls: ['./layout.component.scss'],
 })
